6fd24709e3393281f236a0028ffe303d747827ec
Zeeshan Hussain <zeeshan1414@gmail.com>
2021-01-04 09:52:51 +0530

    Initial Commit

---
+++ basic_api/serializers.py
@@ -0,0 +1,9 @@
+#basic_api/serializers.py
+
+from rest_framework import serializers
+from basic_api.models import DRFPost
+
+class DRFPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DRFPost
+        fields = '__all__'
